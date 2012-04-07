69ae2fd0e6a2f4d3bd227511498fc0fed2b62b73
Max <silverhammermba@gmail.com>
2012-04-07 17:40:25 -0400

    Moved FPS display inside window. Neighbors of cows set on a separate counter.

--- wrangle.cpp
+++ wrangle.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <cstdlib>
 #include <SFML/Graphics.hpp>
 #include "cow.hpp"
@@ -8,13 +9,23 @@ using std::cerr;
 using std::endl;
 
 const float AI_INTERVAL = 0.5f;
+const float DIST_INTERVAL = 5;
 
 int main(int argc, char *argv[])
 {
+	// parse command line
+	const int COWS = (argc == 2 ? atoi(argv[1]) : 1);
+
+	// create objects
 	srand((unsigned)time(0));
 
 	sf::Clock clock;
 	sf::Clock ai;
+	sf::Clock dist;
+
+	sf::Text fps;
+	fps.setCharacterSize(12);
+	std::ostringstream fps_s;
 
 	sf::RenderWindow window
 	{
@@ -26,8 +37,7 @@ int main(int argc, char *argv[])
 
 	sf::View view (window.getView());
 
-	const int COWS = (argc == 2 ? atoi(argv[1]) : 1);
-
+	// set up the cows TODO would be nice to move this to the contstructor
 	Cow *cows = new Cow[COWS];
 	for(Cow *cow = cows; cow != cows + COWS; cow++)
 	{
@@ -35,8 +45,10 @@ int main(int argc, char *argv[])
 		cow->think();
 	}
 
+	// game loop
 	while (window.isOpen())
 	{
+		// process events
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
@@ -46,29 +58,58 @@ int main(int argc, char *argv[])
 				window.close();
 			else if (event.type == sf::Event::Resized)
 			{
-				view.setSize((sf::Vector2f)window.getSize());
+				view.setSize(sf::Vector2f(event.size.width, event.size.height));
 				window.setView(view);
+				fps.setPosition(view.getCenter() - view.getSize() / 2.f);
 			}
 		}
 
 		float time = clock.getElapsedTime().asSeconds();
 		clock.restart();
 
+		if(dist.getElapsedTime().asSeconds() >= DIST_INTERVAL)
+		{
+			dist.restart();
+			for (Cow *cow = cows; cow != cows + COWS; cow++)
+				cow->resetN();
+			
+			for (Cow *cow1 = cows; cow1 != cows + COWS - 1; cow1++)
+			{
+				for (Cow *cow2 = cow1 + 1; cow2 != cows + COWS; cow2++)
+				{
+					if (v2dist(cow1->pos(), cow2->pos()) <= Cow::D_THRESHOLD)
+					{
+						cow1->addCow(cow2);
+						cow2->addCow(cow1);
+					}
+				}
+			}
+		}
+
+		// AI
 		if(ai.getElapsedTime().asSeconds() >= AI_INTERVAL)
 		{
 			ai.restart();
 			for(Cow *cow = cows; cow != cows + COWS; cow++)
 				cow->think();
-			cerr << 1.f / time << " fps\n";
+			// TODO get a proper clock for this
+			fps_s.str("");
+			fps_s << 1.f / time << " FPS";
+			fps.setString(fps_s.str());
 		}
 
+
+		// update entities
 		for(Cow *cow = cows; cow != cows + COWS; cow++)
 			cow->step(time);
 
+		// draw everything
 		window.clear(sf::Color(46, 196, 255));
 
 		for(Cow *cow = cows; cow != cows + COWS; cow++)
 			cow->draw(window);
+
+		window.draw(fps);
 
 		window.display();
 	}
