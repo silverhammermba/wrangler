93b0f9f052219b090432e6bc5db52c5659120080
Max <silverhammermba@gmail.com>
2012-04-07 17:39:28 -0400

    Added distance function for sf::Vector2f

--- helpers.hpp
+++ helpers.hpp
@@ -1,6 +1,9 @@
 #ifndef WRANGLE_HELPERS_H_
 #define WRANGLE_HELPERS_H_
 
+#include <cmath>
+#include <SFML/System.hpp>
+
 // like fmod, but returns a number in [-d/2,d/2]
 float fmodp(float n, float d);
 
@@ -11,6 +14,10 @@ T clamp(T lb, T x, T ub);
 // generate a random number in a range
 template <class T>
 T randm(T max);
+
+// find distance between two points as sf::Vector2<T>
+template <class T>
+float v2dist(sf::Vector2<T>, sf::Vector2<T>);
 
 // constrain x to [lb, ub]
 template <class T>
@@ -24,6 +31,12 @@ template <class T>
 T randm(T max)
 {
 	return (T)(max * ((float)rand() / (float)RAND_MAX));
+}
+
+template <class T>
+float v2dist(sf::Vector2<T> v1, sf::Vector2<T> v2)
+{
+	return sqrt((float)((v1.x - v2.x) * (v1.x - v2.x) + (v1.y - v2.y) * (v1.y - v2.y)));
 }
 
 #endif
